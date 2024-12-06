749a3aca0f3333d00777fda0884cb9af467db2b9
medetisrafilov <israfilovmedet52@gmail.com>
2024-12-05 16:33:07 -1200

    homework

---
+++ abb.js
@@ -0,0 +1,19 @@
+const originalMassiv = [1, 2, 3, 4, 5, ];
+
+const yeniMassiv= [originalMassiv];
+
+for (let i = 0; i< yeniMassiv.length; i++);
+yeniMassiv[i] *=2
+
+
+console.log("Original massiv:", originalMassiv);
+console.log("Yeni massiv:", YeniMassiv);
+
+
+console.log("Yeni massiv:", yeniMassiv);
+
+
+console.log(
+    "original massiv deyismezdir"
+    // JSON.stringify([1, 2, 3, 4, 5, ])
+);
